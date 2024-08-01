4a4c9f709df63e1a354d50a5adffe2a5b9b39e95
nrmndev <nrmndev@gmail.com>
2024-08-02 01:55:39 +1000

    Update Button Component

--- src/components/button/Index.tsx
+++ src/components/button/Index.tsx
@@ -1,5 +1,13 @@
 const Button = () => {
-  return <button className="btn btn--primary btn--bordered">Button</button>;
+  return (
+    <>
+      <button className="btn btn--primary">Button Primary</button>
+      <button className="btn btn--block btn--primary">
+        Button Block Primary
+      </button>
+      <button className="btn btn--large">Button Large</button>
+    </>
+  );
 };
 
 export default Button;
