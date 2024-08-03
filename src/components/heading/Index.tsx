036f09f3f15da863b18538b1e0216e13eff559d8
nrmndev <nrmndev@gmail.com>
2024-08-04 05:56:28 +1000

    Update Heading to conform with SpreadClassNameAsProps Utility

--- src/components/heading/Index.tsx
+++ src/components/heading/Index.tsx
@@ -1,28 +1,24 @@
 import { TextTransform } from "@components/modifiers/textTransform";
+import { SpreadClassNameAsProps } from "../../utilities/SpreadClassNameAsProps";
 
 interface IProps {
   level: 1 | 2 | 3 | 4 | 5 | 6;
   children: React.ReactNode;
-  maxCharacters?: 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80;
-  textTransform?: TextTransform;
+  maxCharacters?: 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | "";
+  textTransform?: TextTransform | "";
 }
 export const Heading = ({
   level,
   children,
-  maxCharacters,
-  textTransform,
+  maxCharacters = "",
+  textTransform = "",
 }: IProps) => {
   const Tag = `h${level}` as keyof JSX.IntrinsicElements;
 
-  const classNames: string[] = [];
-  maxCharacters && classNames.push(`width-ch` + maxCharacters);
-  textTransform && classNames.push(`text-` + textTransform);
+  const classNames = SpreadClassNameAsProps([
+    ["", maxCharacters && "width-ch" + maxCharacters.toString()],
+    ["", textTransform && "text-" + textTransform],
+  ]);
 
-  const props = {
-    ...(classNames.length > 0 && { className: classNames.join(" ") }),
-  };
-  //   maxCharacters && classNames.push(`width-ch` + maxCharacters);
-  //   textTransform && classNames.push(`text-` + textTransform);
-
-  return <Tag {...props}>{children}</Tag>;
+  return <Tag {...classNames}>{children}</Tag>;
 };
