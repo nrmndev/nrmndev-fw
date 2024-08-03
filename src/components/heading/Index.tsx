365623b77e79ce95be92bec4424236ec499ff9c3
nrmndev <nrmndev@gmail.com>
2024-08-04 07:49:29 +1000

    revised width approach from ch to %

--- src/components/heading/Index.tsx
+++ src/components/heading/Index.tsx
@@ -1,22 +1,25 @@
 import { TextTransform } from "@components/modifiers/textTransform";
-import { SpreadClassNameAsProps } from "../../utilities/SpreadClassNameAsProps";
+import { SpreadClassNameAsProps } from "@utils/SpreadClassNameAsProps";
 
 interface IProps {
   level: 1 | 2 | 3 | 4 | 5 | 6;
   children: React.ReactNode;
-  maxCharacters?: 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | "";
+  //maxCharacters?: 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | "";
+  width?: 25 | 33 | 50 | 66 | 75 | 80 | "";
   textTransform?: TextTransform | "";
 }
 export const Heading = ({
   level,
   children,
-  maxCharacters = "",
+  //maxCharacters = "",
+  width = "",
   textTransform = "",
 }: IProps) => {
   const Tag = `h${level}` as keyof JSX.IntrinsicElements;
 
   const classNames = SpreadClassNameAsProps([
-    ["", maxCharacters && "width-ch" + maxCharacters.toString()],
+    //["", maxCharacters && "width-ch" + maxCharacters.toString()],
+    ["", width && "width-" + width.toString()],
     ["", textTransform && "text-" + textTransform],
   ]);
 
