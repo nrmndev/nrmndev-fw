d651cbf87c7567fcfac52b57703a821a429eb02e
nrmndev <nrmndev@gmail.com>
2024-08-04 05:54:50 +1000

    Update button to conform with SpreadClassNameAsProps Utility

--- src/components/button/Index.tsx
+++ src/components/button/Index.tsx
@@ -1,22 +1,31 @@
+import { SpreadClassNameAsProps } from "../../utilities/SpreadClassNameAsProps";
+
 interface IProps {
   variant: "primary" | "primary-outlined" | "secondary" | "secondary-outlined";
-  display?: "block" | "inline-block";
-  size?: "small" | "medium" | "large" | "x-large";
+  display?: "block" | "inline-block" | "";
+  size?: "small" | "medium" | "large" | "x-large" | "";
   children: React.ReactNode;
 }
 
-const Button = ({ variant = "primary", display, size, children }: IProps) => {
-  const classNames: string[] = ["btn"];
-  classNames.push(`btn--` + variant);
-  display && classNames.push(`btn--` + display);
-  size && classNames.push(`btn--` + size);
-  const props = {
-    ...(classNames.length > 0 && { className: classNames.join(" ") }),
-  };
+const Button = ({
+  variant = "primary",
+  display = "",
+  size = "",
+  children,
+}: IProps) => {
+  // const classNames: string[] = ["btn"];
+  // classNames.push(`btn--` + variant);
+  // display && classNames.push(`btn--` + display);
+  // size && classNames.push(`btn--` + size);
+  // const props = {
+  //   ...(classNames.length > 0 && { className: classNames.join(" ") }),
+  // };
 
+  const classNames = SpreadClassNameAsProps(["btn", variant, display, size]);
+  //console.log(props);
   return (
     <>
-      <button {...props}>{children}</button>
+      <button {...classNames}>{children}</button>
     </>
   );
 };
