96e0710785e535d3b499891ec9578c9c63f9f157
nrmndev <nrmndev@gmail.com>
2024-08-04 07:50:19 +1000

    Added ImagePath support, and variants and rounded props

--- src/components/icon/Index.tsx
+++ src/components/icon/Index.tsx
@@ -1,26 +1,41 @@
+import { SpreadClassNameAsProps } from "@utils/SpreadClassNameAsProps";
 import { IconType } from "react-icons";
 import { ImCogs } from "react-icons/im";
 
 interface IProps {
-  icon: IconType;
-  variant: "primary-rounded" | "secondary-rounded" | "primary" | "secondary";
+  icon: IconType | string;
+  variant:
+    | "primary"
+    | "secondary"
+    | "primary-outline"
+    | "secondary-outline"
+    | "primary-alt"
+    | "secondary-alt";
+  rounded?: boolean;
 }
 
-const Icon = ({ icon: Icon = ImCogs, variant = "primary" }: IProps) => {
-  const classNames: string[] = ["icon"];
-  classNames.push(`icon--` + variant);
-  variant && classNames.push(`icon--` + variant);
-  // size && classNames.push(`icon--` + size);
-  const props = {
-    ...(classNames.length > 0 && { className: classNames.join(" ") }),
-  };
+/**
+ *
+ * @param {IconType | string} icon React-icons or {PathToImage}
+ * @param {"primary" | "secondary | "primary-outline | "secondary-outline"} variant Icon Skin
+ * @returns Icon Element
+ */
+
+const Icon = ({
+  icon: Icon = ImCogs,
+  variant = "primary",
+  rounded = false,
+}: IProps) => {
+  const props = SpreadClassNameAsProps([
+    "icon",
+    variant,
+    rounded ? "rounded" : "",
+  ]);
 
   return (
-    <>
-      <span {...props}>
-        <Icon />
-      </span>
-    </>
+    <span {...props}>
+      {typeof Icon === "string" ? <img src={Icon} /> : <Icon />}
+    </span>
   );
 };
 
