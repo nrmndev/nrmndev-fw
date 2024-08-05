4d859c6fd380b2858399325ee268eb5e2aa7ce2e
nrmndev <nrmndev@gmail.com>
2024-08-06 03:15:34 +1000

    Added HTMLProps for HTMLHeadingElement, merged with props classes

--- src/components/heading/Heading.tsx
+++ src/components/heading/Heading.tsx
@@ -1,7 +1,7 @@
-import type { TextTransform } from "types/Index";
-import { SpreadClassNameAsProps } from "@utils/SpreadClassNameAsProps";
+import { TextTransform } from "@typekits";
+import { MergePropsAsClassNames } from "@utils";
 
-interface IProps extends TextTransform {
+interface IProps extends React.HTMLProps<HTMLHeadingElement>, TextTransform {
   children: React.ReactNode;
   level: 1 | 2 | 3 | 4 | 5 | 6;
   width?: 25 | 33 | 50 | 66 | 75 | 80 | "";
@@ -12,17 +12,19 @@ const Heading = ({
   level,
   textTransform = "",
   width = "",
+  ...rest
 }: //maxCharacters = "",
 IProps) => {
   const Tag = `h${level}` as keyof JSX.IntrinsicElements;
 
-  const classNames = SpreadClassNameAsProps([
-    //["", maxCharacters && "width-ch" + maxCharacters.toString()],
+  const classNames = MergePropsAsClassNames([
     ["", width && "width-" + width.toString()],
     ["", textTransform && "text-" + textTransform],
+    [rest.className ? rest.className : ""],
+    //["", maxCharacters && "width-ch" + maxCharacters.toString()],
   ]);
 
-  return <Tag {...classNames}>{children}</Tag>;
+  return <Tag className={classNames}>{children}</Tag>;
 };
 
 export default Heading;
