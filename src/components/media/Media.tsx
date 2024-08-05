907d8df7ab50407a068c13bd9ade066ea9e88a91
nrmndev <nrmndev@gmail.com>
2024-08-06 03:17:04 +1000

    Added HTMLProps for HTMLDivElement, added Block/Element for Icon usage

--- src/components/media/Media.tsx
+++ src/components/media/Media.tsx
@@ -1,25 +1,35 @@
-import { IconBaseProps } from "types/Index";
+import { IconBaseProps } from "@typekits";
 import { Icon } from "@components";
 
-interface IProps extends IconBaseProps {
+interface IProps
+  extends React.HTMLProps<HTMLDivElement>,
+    Omit<IconBaseProps, "rounded"> {
   heading: string;
   body: JSX.Element;
   link?: string;
   target?: "_blank" | "_self" | "_top" | "_parent";
+  roundedIcon?: boolean;
 }
 
 const Media = ({
   link,
   target = "_self",
   icon: IconToRender,
+  roundedIcon = false,
   heading,
   body,
+  ...rest
 }: IProps) => {
   if (link)
     return (
-      <div className="media">
+      <div className={`media ${rest.className ? rest.className : ""}`}>
         <a className="media__link" href={link} target={target}>
-          <Icon icon={IconToRender} variant="primary" />
+          <Icon
+            icon={IconToRender}
+            variant="primary"
+            rounded={roundedIcon}
+            className="icon__media"
+          />
           <div className="media__content">
             <h4 className="media__heading">{heading}</h4>
             <p className="media__body">{body}</p>
@@ -29,8 +39,8 @@ const Media = ({
     );
   else
     return (
-      <div className="media">
-        <Icon icon={IconToRender} variant="primary" />
+      <div className={`media ${rest.className ? rest.className : ""}`}>
+        <Icon icon={IconToRender} variant="primary" rounded={roundedIcon} />
         <div className="media__content">
           <h4 className="media__heading">{heading}</h4>
           <p className="media__body">{body}</p>
