e77ec6ea439dab78373c9f41410e767871d72df2
nrmndev <nrmndev@gmail.com>
2024-08-06 03:14:09 +1000

    Added HTMLProps for HTMLButtonElement, merged to props

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,9 +1,10 @@
-import { SpreadClassNameAsProps } from "@utils/SpreadClassNameAsProps";
+import { MergePropsAsClassNames } from "@utils";
 
-interface IProps {
+interface IProps extends Omit<React.HTMLProps<HTMLButtonElement>, "size"> {
   variant: "primary" | "primary-outlined" | "secondary" | "secondary-outlined";
   display?: "block" | "inline-block" | "";
   size?: "small" | "medium" | "large" | "x-large" | "";
+  type?: "submit" | "button" | "reset";
   children: React.ReactNode;
 }
 
@@ -11,13 +12,20 @@ const Button = ({
   variant = "primary",
   display = "",
   size = "",
+  type = "button",
   children,
+  ...rest
 }: IProps) => {
-  const classNames = SpreadClassNameAsProps(["btn", variant, display, size]);
+  const classNames = MergePropsAsClassNames([
+    ["btn", variant, display, size],
+    [rest.className ? rest.className : ""],
+  ]);
 
   return (
     <>
-      <button {...classNames}>{children}</button>
+      <button {...rest} className={classNames} type={type}>
+        {children}
+      </button>
     </>
   );
 };
