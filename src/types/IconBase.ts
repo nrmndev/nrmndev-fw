6363c40b18a7962649adbb959c42a415ab16ea35
nrmndev <nrmndev@gmail.com>
2024-08-05 19:11:04 +1000

    Created reusable types, IconBase and TextTransform

---
+++ src/types/IconBase.ts
@@ -0,0 +1,5 @@
+import { IconType } from "react-icons";
+
+export interface IconBaseProps {
+  icon: IconType | string;
+}
---
+++ src/types/TextTransform.ts
@@ -0,0 +1,3 @@
+export interface TextTransform {
+  textTransform?: "uppercase" | "capitalize" | "lowercase" | "";
+}
