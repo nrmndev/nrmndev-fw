bf13bb3ac3e7732f98bb2994b1f5d80d96b8e0e9
nrmndev <nrmndev@gmail.com>
2024-08-12 22:50:22 +1000

    Created Text component using a styled-component

--- src/components/text/Text.tsx
+++ src/components/text/Text.tsx
@@ -1,13 +1,37 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledP = styled.p<{ $maxChars: number | "" }>`
+  ${({ $maxChars }) =>
+    $maxChars
+      ? `
+        max-width: ${$maxChars + "ch"};
+        display: block;
+        overflow: hidden; /* Hide overflowed text */
+        text-overflow: ellipsis; /* Add ellipsis for overflowed text */
+        white-space: nowrap; /* Prevent text from wrapping */
+    `
+      : "max-width:auto"};
+`;
+
+// const StyledSpan = styled.span<{ maxChars: number }>`
+//   width: ${(maxCharss) =>
+//     maxCharss ? maxCharss.toString() + "ch" : "auto"};
+// `;
 
 interface IProps
   extends React.HTMLProps<HTMLParagraphElement | HTMLSpanElement> {
-  maxCharacters?: number;
+  maxChars?: number | "";
   children?: React.ReactNode;
+  as?: "span" | "p";
 }
 
-const Text = ({ maxCharacters }: IProps) => {
-  return <div>Text</div>;
+const Text = ({ maxChars = -1, as = "p", children }: IProps) => {
+  return (
+    <StyledP as={as} $maxChars={maxChars}>
+      {children}
+    </StyledP>
+  );
 };
 
 export default Text;
---
+++ src/routing/SampleComponents/SampleText.tsx
@@ -0,0 +1,11 @@
+import { Text } from "@components";
+
+const SampleText = () => {
+  return (
+    <Text as="span" maxChars={50}>
+      Hello
+    </Text>
+  );
+};
+
+export default SampleText;
