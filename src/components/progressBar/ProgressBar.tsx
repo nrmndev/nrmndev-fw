5a0d92393bfa687757ad5b94a0532527dd0ba12e
nrmndev <nrmndev@gmail.com>
2024-08-14 00:46:26 +1000

    Created ProgressBar component

---
+++ src/components/progressBar/ProgressBar.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+interface IProps {
+  totalProgress: number;
+  currentProgress: number;
+  showProgress?: boolean;
+}
+
+const StyledSpan = styled.span<{
+  $currentProgress: number;
+  $totalProgress: number;
+}>`
+  ${({ $currentProgress, $totalProgress }) => `
+    ${
+      $currentProgress & $currentProgress
+        ? `width: ${($currentProgress / $totalProgress) * 100}%;`
+        : ""
+    }
+  `}
+`;
+
+const ProgressBar = ({
+  totalProgress,
+  currentProgress,
+  showProgress = false,
+}: IProps) => {
+  return (
+    <span className="progress-bar">
+      <StyledSpan
+        className="progress-bar__fill"
+        $totalProgress={totalProgress}
+        $currentProgress={currentProgress}
+      >
+        {showProgress && currentProgress}
+      </StyledSpan>
+    </span>
+  );
+};
+
+export default ProgressBar;
