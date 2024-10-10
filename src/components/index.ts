971baa2082b9ba0d6c09c205facfd56757a8e0e2
nrmndev <nrmndev@gmail.com>
2024-10-10 21:46:27 +1100

    renamed container export to layout

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export * from "./button";
 export * from "./card";
-export * from "./container";
+export * from "./layout";
 export * from "./form";
 export * from "./group";
 export * from "./hero";
