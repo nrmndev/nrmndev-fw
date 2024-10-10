5ec6b7326343c552e8abd121d16022aea920d171
nrmndev <nrmndev@gmail.com>
2024-10-10 21:44:33 +1100

    renamed container to layout, added row component, restructure SCSS files

---
+++ src/components/layout/Row.tsx
@@ -0,0 +1,30 @@
+import { BaseProps, FlexColumnProps } from "@uiTypes";
+import { propStyleHandler } from "@utils";
+import classNames from "classnames";
+import flexColumns from "utils/propStyleHandler/asClassName/flexColumns";
+
+export interface IPropsRow extends BaseProps, FlexColumnProps {}
+
+const Row = ({
+  xsColumns,
+  smColumns,
+  mdColumns,
+  lgColumns,
+  xlColumns,
+  xxlColumns,
+  ...rest
+}: IPropsRow) => {
+  const flexColumns = {
+    xsColumns,
+    smColumns,
+    mdColumns,
+    lgColumns,
+    xlColumns,
+    xxlColumns,
+  };
+  const { className } = propStyleHandler({ flexColumns });
+  const classes = classNames("row", flexColumns && `flex`, className);
+  return <div {...rest} className={classes}></div>;
+};
+
+export default Row;
