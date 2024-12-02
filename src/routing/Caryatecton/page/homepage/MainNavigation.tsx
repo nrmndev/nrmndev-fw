7ad9056e55c17e598b6b70d790ecde11236b08cc
nrmndev <nrmndev@gmail.com>
2024-12-02 15:52:33 +1100

    added main navigation

--- src/routing/Caryatecton/page/homepage/MainNavigation.tsx
+++ src/routing/Caryatecton/page/homepage/MainNavigation.tsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Link } from "react-scroll";
 import logo from "../../../../assets/caryatectonLogo.png";
 import { Image } from "@uiComponents";
+import { GiHamburgerMenu } from "react-icons/gi";
 const MainNavigation = () => {
+  const [navActive, setNavActive] = useState(false);
+
   return (
     <div className="main-nav-wrapper">
       <ul className="main-nav">
-        <li>
+        <li style={{ display: navActive ? "block" : "none" }}>
           <Link to="trades" spy={true} smooth={true} offset={50} duration={500}>
             Trades
           </Link>
         </li>
-        <li>
+        <li style={{ display: navActive ? "block" : "none" }}>
           <Link
             to="services"
             spy={true}
@@ -27,7 +30,7 @@ const MainNavigation = () => {
             <Image src={logo} className="main-logo" />
           </Link>
         </li>
-        <li>
+        <li style={{ display: navActive ? "block" : "none" }}>
           <Link
             to="projects"
             spy={true}
@@ -38,7 +41,7 @@ const MainNavigation = () => {
             Projects
           </Link>
         </li>
-        <li>
+        <li style={{ display: navActive ? "block" : "none" }}>
           <Link
             to="contact"
             spy={true}
@@ -50,6 +53,12 @@ const MainNavigation = () => {
           </Link>
         </li>
       </ul>
+      <button
+        className="main-nav-mobile-toggle"
+        onClick={() => setNavActive(!navActive)}
+      >
+        <GiHamburgerMenu />
+      </button>
     </div>
   );
 };
